df71936325da3d2d1721daa6a061e693ae751242
Pero Maric <peromaric91@gmail.com>
2022-10-04 19:39:38 +0200

    add wallet create component

---
+++ web/src/app/wallet-create/wallet-create.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WalletCreateComponent } from './wallet-create.component';
+
+describe('WalletCreateComponent', () => {
+  let component: WalletCreateComponent;
+  let fixture: ComponentFixture<WalletCreateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WalletCreateComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WalletCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web/src/app/wallet-create/wallet-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { WalletService} from '../services/wallet.service'
+
+
+
+@Component({
+  selector: 'app-wallet-create',
+  templateUrl: './wallet-create.component.html',
+  styleUrls: ['./wallet-create.component.css']
+})
+export class WalletCreateComponent implements OnInit {
+  walletSecretKey = new FormControl('');
+
+  constructor(
+    private walletService: WalletService
+  ) { }
+  
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.walletService.registerWallet(this.walletSecretKey.value).subscribe(() => this.walletService.getWallets()));
+  }
+}
